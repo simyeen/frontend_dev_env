e0bd8cbd435f7915984eb7b8f132509c10fd8fb5
simyeen <simyeen@naver.com>
2023-08-02 16:29:30 +0900

    feat: adding babel.config.js and use that plugin, preset.js

---
+++ babel.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  preset: ["./my-babel-preset.js"],
+};
--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import * as math from "./math.js";
-import "./app.css";
-import nyancat from "./nyancat.jpeg";
+// import * as math from "./math.js";
+// import "./app.css";
+// import nyancat from "./nyancat.jpeg";
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = `<img src="${nyancat}"/>`;
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.body.innerHTML = `<img src="${nyancat}"/>`;
+// });
 
-console.log(process.env.NODE_ENV);
-console.log(TWO);
+// console.log(process.env.NODE_ENV);
+// console.log(TWO);
